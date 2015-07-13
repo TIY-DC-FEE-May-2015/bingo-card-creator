32b0929e31899419c6f1c6ae0ba646221ab25360
Steve Koo <shkoo25@gmail.com>
2015-07-13 17:13:56 -0400

    Bingo Card

--- public/card.js
+++ public/card.js
@@ -18,13 +18,24 @@ app.controller("CardController", function($scope){
     switch($scope.cardRule) {
       case "line":
         straightLine()
+        break;
+
       case "fullCard":
+        fullCard()
+        break;
 
       case "corners":
+      cornerCard()
+        break;
 
       case "outline":
+        outline()
+        break;
+
 
       case "hashtag":
+        hashtagCard()
+        break;
       
     }
   }
@@ -32,7 +43,7 @@ app.controller("CardController", function($scope){
   var straightLine = function() {
     var cols = _.reduce($scope.board, function(memory, row){
       return _.map(row, function(cell, index){
-        if (memory[index] === 0) {
+        if (memory[index] === 0) { 
           return 0
         }
         return cell
@@ -40,6 +51,7 @@ app.controller("CardController", function($scope){
     }, [ 1, 1, 1, 1, 1 ])
 
     var matchInColumns = _.some(cols)
+    console.log(matchInColumns)
 
     var matchInRows = _.some($scope.board, function(row){
       return _.every(row)
@@ -66,6 +78,71 @@ app.controller("CardController", function($scope){
     if (matchInColumns || matchInRows || matchInDiagonal) {
       $scope.winner = true
     }
+  }
+
+  var fullCard = function() {
+    console.log($scope.winner = _.every( _.flatten($scope.board) ) )
+  }
+
+  var outline = function(){ 
+      var cols = _.reduce($scope.board, function(memory, row){
+      return _.map(row, function(cell, index){
+        if (memory[index] === 0) {
+          return 0
+        }
+        return cell
+      })
+    }, [ 1, 1, 1, 1, 1 ])
+
+   
+
+      var row = _.every($scope.board[0], _.identity)
+      var column = _.every($scope.board[4], _.identity)
+      console.log(row, column)
+
+      if (row && column) {
+       $scope.winner = true
+      }
+  }    
+    
+
+
+  var cornerCard = function(){
+
+    var corners = 1
+    _.times(5, function(i){
+      if (corners === 0) {
+        return 0
+    }
+    corners = $scope.board[0][0] && $scope.board[0][4] && $scope.board[4][0] && $scope.board[4][4]
+    })
+
+    if (corners){
+      $scope.winner = true
+    }
+      
+  }
+        
+
+  var hashtagCard= function(){
+     var cols = _.reduce($scope.board, function(memory, column){
+      return _.map(column, function(cell, index){
+        if (memory[index] === 0) {
+          return 0
+        }
+        return cell
+      })
+    }, [ 1, 1, 1, 1, 1 ])
+
+      var row = _.every($scope.board[1], _.identity)
+      var column = _.every($scope.board[3], _.identity)
+      console.log(row, column)
+
+      if (row && column) {
+       $scope.winner = true
+      }
+
+
   }
 
 })
