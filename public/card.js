9701f192163375063d4630cc6ce7f1fa4bb6da5c
BBSR <scottamross@gmail.com>
2015-07-15 07:31:00 -0400

    add a few new rules, dry out most of the code

--- public/card.js
+++ public/card.js
@@ -12,6 +12,73 @@ app.controller("CardController", function($scope){
 
   $scope.cardRule = "line"
 
+  var checkSpecificColumn = function(index) {
+    if ( _.every($scope.board[index]) ){
+      return true
+    }
+  }
+
+  var checkMultipleColumns = function(arrayOfNumbers) {
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+      if ( !checkSpecificColumn( arrayOfNumbers[i] ) ) {
+        return
+      }
+    }
+    return true
+  }
+
+  var checkSpecificRow = function(index){
+    //because transposed we are going vertically 
+      //for each vertical col, check the number index of index
+    var col = _.map($scope.board, function(col){
+      return col[index]
+    })
+    //if all are true, we good
+    return _.every(col)
+  }
+
+  var checkMultipleRows = function(arrayOfNumbers) {
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+      if ( !checkSpecificRow( arrayOfNumbers[i] ) ) {
+        return
+      }
+    }
+    return true
+/*   return _.every(arrayOfNumbers, function(i){
+      return checkSpecificRow(i)
+    })*/
+  }
+
+  var checkDiagonal = function(str) {
+    var diagonal = 1
+    _.times(5, function(i){
+      if (diagonal === 0) {
+        return 0
+      }
+      if (str === "down") {
+        diagonal = $scope.board[i][i]
+      }
+      if (str === "up"){
+        diagonal = $scope.board[i][4-i]
+      }
+    })
+    return diagonal
+  }
+
+
+  
+/*    var rightDiagonalMatch = 1
+    _.times(5, function(i){
+      if (rightDiagonalMatch === 0) {
+        return 0
+      }
+      rightDiagonalMatch = $scope.board[i][4 - i]
+    })
+
+    var matchInDiagonal = (leftDiagonalMatch || rightDiagonalMatch)*/
+
+
+
   $scope.clickSquare = function(squareRow, squareColumn) {
     $scope.board[squareRow][squareColumn] = 1
 
@@ -31,87 +98,41 @@ app.controller("CardController", function($scope){
       case "hashtag":
         hashtag()
         break
+      case "prisonBars":
+        prisonBars()
+        break
+      case "teamZissou":
+        teamZissou()
+        break
     }
   }
 
-  var hashtag = function(){
-    var matchInColumns = _.some($scope.board, function(row){
-      var leftCol =  _.every($scope.board[1])
-      var rightCol =  _.every($scope.board[3])
-      if (leftCol && rightCol) {
-        console.log("col")
-        return true
+
+
+
+
+
+  var teamZissou = function(){
+    var diagonalMatch = checkDiagonal("up") 
+    if ( checkMultipleRows([0, 4]) && diagonalMatch ) {
+      $scope.winner=true
+    }
+  }
+
+  var prisonBars = function(){
+      if( checkMultipleColumns([0,2,4]) ) {
+        $scope.winner = true
       }
-    })
+  }
 
-    var matchInRows = _.reduce($scope.board, function(memory, row){
-      return _.map(row, function(cell, index){
-        if (index === 1){ 
-          if (memory[index] === 0 ) {
-            return 0
-          }
-          return cell
-        }
-
-       if (index === 3){ 
-        if (memory[index] === 0 ) {
-            return 0
-          }
-          return cell
-        }        
-      })
-
-    }, [1,1,1,1,1])
-
-    rowTop = matchInRows[1] 
-    rowBottom = matchInRows[3]
-
-    if (rowTop && rowBottom && matchInColumns) {
+  var hashtag = function(){
+    if ( checkMultipleRows([1, 3]) && checkMultipleColumns([1, 3]) ) {
       $scope.winner = true
     }
   }
 
   var outline = function(){
-  
-//row and column are reversed dude
-    var sideCols = _.reduce($scope.board, function(memory, row){
-      return _.map(row, function(cell, index){
-        if (index === 0){ 
-          if (memory[index] === 0 ) {
-            return 0
-          }
-          return cell
-        }
-
-       if (index === 4){ 
-        if (memory[index] === 0 ) {
-            return 0
-          }
-          return cell
-        }        
-      })
-
-    }, [1,1,1,1,1])
-
-
-    var columnLeft = sideCols[0]
-    var columnRight = sideCols[4]
-
-    var matchInRows = _.some($scope.board, function(row){
-      //makes it the top
-      //return row[0]
-      //$scope.board makes it the column... for reasons i don't understand
-      var leftRow =  _.every($scope.board[0])
-      var rightRow =  _.every($scope.board[4])
-      if (leftRow && rightRow) {
-        return true
-      }
-    })
-
-    console.log( matchInRows)
-
-
-    if (columnLeft && columnRight && matchInRows) {
+    if ( checkMultipleRows([0,4]) && checkMultipleColumns([0,4]) ) {
       $scope.winner = true
     }
   }
@@ -181,23 +202,7 @@ app.controller("CardController", function($scope){
       return _.every(row)
     })
 
-    var leftDiagonalMatch = 1
-    _.times(5, function(i){
-      if (leftDiagonalMatch === 0) {
-        return 0
-      }
-      leftDiagonalMatch = $scope.board[i][i]
-    })
-
-    var rightDiagonalMatch = 1
-    _.times(5, function(i){
-      if (rightDiagonalMatch === 0) {
-        return 0
-      }
-      rightDiagonalMatch = $scope.board[i][4 - i]
-    })
-
-    var matchInDiagonal = (leftDiagonalMatch || rightDiagonalMatch)
+    var matchInDiagonal = ( checkDiagonal("up") || checkDiagonal("down") )
 
     if (matchInColumns || matchInRows || matchInDiagonal) {
       $scope.winner = true
@@ -205,3 +210,37 @@ app.controller("CardController", function($scope){
   }
 
 })
+
+
+//removed from var hashtag:
+/*    var matchInColumns = _.some($scope.board, function(row){
+      var leftCol =  _.every($scope.board[1])
+      var rightCol =  _.every($scope.board[3])
+      if (leftCol && rightCol) {
+        console.log("col")
+        return true
+      }
+
+    var matchInRows = _.reduce($scope.board, function(memory, row){
+      return _.map(row, function(cell, index){
+        if (index === 1){ 
+          if (memory[index] === 0 ) {
+            return 0
+          }
+          return cell
+        }
+
+       if (index === 3){ 
+        if (memory[index] === 0 ) {
+            return 0
+          }
+          return cell
+        }        
+      })
+
+    }, [1,1,1,1,1])
+
+    rowTop = matchInRows[1] 
+    rowBottom = matchInRows[3]
+    })
+  */
